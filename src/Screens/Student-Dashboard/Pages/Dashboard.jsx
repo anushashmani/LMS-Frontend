235c66f41e3b34acb9b0848d8038561a886ba779
Anus Shafi <anushashmani10@gmail.com>
2025-01-30 14:36:08 +0500

    updated

--- src/Screens/Student-Dashboard/Pages/Dashboard.jsx
+++ src/Screens/Student-Dashboard/Pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarImage } from "@/Components/ui/avatar";
 import FinanceSection from "../Compunents/FinanceSection";
 import { EnrolledCourses } from "../Compunents/EnrolledCourses";
 import { useAuth } from "@/Context/AuthContext";
